28fd15ec597333445202f43b91351545b851040d
Bekzat Karayev <71756405+bekzatkarayev@users.noreply.github.com>
2020-11-17 15:18:45 +0600

    Upload Task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task01
+{
+    public class Program
+    {
+        public static double FindNthRoot(double number, int degree, double precision)
+        {
+            double guessResult = number / 2;
+            double expectedResult = Math.Pow(number, 1.0 / degree);
+            double result;
+
+            while (Math.Abs(expectedResult - guessResult) > precision)
+            {
+                result = guessResult - Math.Pow(guessResult, degree) / (degree * Math.Pow(guessResult, degree));
+                guessResult = result;
+            }
+
+            return 0;
+        }
+
+        static void Main()
+        {
+        }
+    }
+}
